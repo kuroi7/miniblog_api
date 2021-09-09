923383ff9b567533518667eb196fb43d953a8ab2
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2021-09-09 23:20:32 +0900

    postテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_104944) do
+ActiveRecord::Schema.define(version: 2021_09_08_134746) do
 
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
